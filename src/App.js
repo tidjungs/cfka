7eb54862f35402e51d06ee5b87f57f114b25e698
kaisukez <zykigix@hotmail.com>
2017-10-28 04:48:03 +0700

    add facebook login with react library

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
+import FB from 'fb';
+import FacebookLogin from 'react-facebook-login';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  responseFacebook(response) {
+    console.log(response);
+  }
+
   render() {
     return (
       <div className="App">
@@ -13,6 +19,12 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <FacebookLogin
+          appId="132412297417604"
+          autoLoad={true}
+          fields="name,email,picture"
+          callback={this.responseFacebook}
+        />
       </div>
     );
   }
