bcc7ead3c35fdaf49862023a2815432e2aee9ceb
tidjungs <waewprach.s@ku.th>
2017-10-28 03:32:05 +0700

    add script request

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
 import request from 'request';
 
-request('https://graph.facebook.com/v2.10/1924443867832338/feed?access_token=EAACEdEose0cBABznZCDUScAGClvf57kIe8mBLfA3ZCGLne6aU4G4OmJQ1xBy7Ta3HGPV6KYUxNVgek4h2dUvjndHgZAZAaUH8eZBeJRYwZBzDZBHHpUHjk4Qdm6jzdE9ghREfCm6jGBguHZBK2VXlrK5gg4l927Seq6BgcCfdaxTFZBsTdHmWxnzl4iBmU1h2dMAZD', function (error, response, body) {
-  // console.log('error:', error); 
-  // console.log('statusCode:', response && response.statusCode);
-  console.log('body:', body); 
-});
+const keyword = 'dmsakldnsakldnskal';
+const access_token = 'EAAB5ij8RTdUBAB8tda1qi6OPbJOEdyhOAVzcJqsSYNPhEm1HQpPoN1WEHKRChC9S005ZClUDF5c6Ylx7SYw8uPOZBeukm466P2EjQzGoNWqZAmHy7x3e1T5KrIvleqYHqAvdHXGoFnSm6et5Jx9xJAhv6M3hzMZD';
+
+const doRequest = url => new Promise((resolve, reject) => {
+  request(url, (err, res, body) => {
+    if (!err && res.statusCode === 200) {
+      resolve(body);
+    } else {
+      reject(err);
+    }
+  });
+})
+
+setInterval(() => {
+  console.log('request');
+  doRequest('https://graph.facebook.com/v2.10/1924443867832338/feed?access_token=' + access_token)
+  .then(body => {
+    const { data } = JSON.parse(body);
+    data.forEach(post => {
+      if (post.message && post.message.indexOf(keyword) !== -1) {
+        console.log(post.message);
+      }
+    });
+  }).catch(err => {
+    console.log(err);
+  })
+}, 2000)
