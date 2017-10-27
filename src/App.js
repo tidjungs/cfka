e3a0ead507995fa3193d33edac89711f99e9df68
kaisukez <zykigix@hotmail.com>
2017-10-28 06:43:14 +0700

    pull

--- src/App.js
+++ src/App.js
@@ -1,12 +1,55 @@
 import React, { Component } from 'react';
 import FB from 'fb';
 import FacebookLogin from 'react-facebook-login';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      userName: ''
+    }
+
+    window.fbAsyncInit = function() {
+      FB.init({
+        appId      : '132412297417604',
+        cookie     : true,
+        xfbml      : true,
+        version    : 'v2.10'
+      });
+    }
+  }
+
+  statusChangeCallback(response) {
+    console.log('statusChangeCallback');
+    console.log(response);
+    if (response.status === 'connected') {
+      console.log('you are already login')
+      this.testAPI();
+    } else {
+      console.log('please login')
+    }
+  }
+
+  checkLoginState() {
+    FB.getLoginStatus(function(response) {
+      this.statusChangeCallback(response);
+    });
+  }
+
+  testAPI() {
+    console.log('Welcome!  Fetching your information.... ');
+    FB.api('/me', function(response) {
+      console.log('Successful login for: ' + response.name);
+    });
+  }
+
   responseFacebook(response) {
     console.log(response);
+    this.setState({userName: response.name})
+    console.log(this.state.userName)
   }
 
   render() {
@@ -22,8 +65,8 @@ class App extends Component {
         <FacebookLogin
           appId="132412297417604"
           autoLoad={true}
-          fields="name,email,picture"
-          callback={this.responseFacebook}
+          fields="name,email"
+          callback={this.responseFacebook.bind(this)}
         />
       </div>
     );
